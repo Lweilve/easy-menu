911843e762273de171b77ee9028ba364dd66c2c0
罗蔚略 <luoweilue@sinobest.cn>
2024-01-12 14:47:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# easy-menu
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+### 测试数据
+
+星期一
+早餐
+三层，肉卷，蒜苗，芹菜，炒粄条，打茶
+午餐，
+鱼，时菜，白萝卜焖鸭，肉碎蒸梅菜
+晚餐
+时菜，蒜苗炒肉卷，红烧鸡翅，（红萝卜，青红椒，木耳，炒肉丝）
+星期二
+早餐，
+芹菜，瘦肉粥，蒸玉米
+午餐
+鱼，时菜，三层焖莲藕，蒸蛋
+晚餐
+时菜，葱炒鱼卷，鸡爪焖花生，三层炒西芹
+星期三
+早餐
+瘦肉，芹菜，干面条汤
+午餐
+鱼，时菜，猪脚焖花生，三层炒腐竹
+晚餐
+时菜，（麻辣香锅，玉米，小原味肠，活虾，芋条）三层炒蒜芯，蒸咸鸭蛋
+星期四
+早餐
+白粥，三层炒咸菜。花卷
+午餐
+时菜
+鱼，时菜，芋头焖排骨，三层炒豆角
+晚餐
+鱼，时菜，红烧三层，芹菜鱼丸
+星期五
+早餐
+蒜苗，虾，瘦肉，芹菜煮生面条
+午餐
+鱼，时菜，牛腩煲白萝卜，卤蛋
---
+++ src/views/HomeView.vue
@@ -0,0 +1,134 @@
+<template>
+  <el-config-provider :locale="locale" size="large">
+    <el-form ref="ruleFormRef" :model="form" :rules="rules" label-width="120px">
+      <el-form-item label="开始日期" prop="dateString">
+        <el-date-picker
+          v-model="form.dateString"
+          type="date"
+          placeholder="请选择开始日期"
+          style="width: 100%"
+        />
+      </el-form-item>
+      <el-form-item label="菜单数据" prop="text">
+        <el-input v-model="form.text" type="textarea" rows="7" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit(ruleFormRef)"
+          >生成</el-button
+        >
+      </el-form-item>
+      <el-form-item label="生成数据">
+        <el-input v-model="form.data" type="textarea" rows="7" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleTo">跳转</el-button>
+      </el-form-item>
+    </el-form>
+  </el-config-provider>
+</template>
+
+<script lang="ts" setup>
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import { ElMessage } from "element-plus";
+import { h, ref, reactive } from "vue";
+
+const ruleFormRef = ref();
+const form = reactive({
+  dateString: "",
+  text: "",
+});
+const rules = reactive({
+  dateString: [
+    { required: true, message: "请选择开始日期", trigger: "change" },
+  ],
+  text: [
+    {
+      required: true,
+      message: "请输入菜单数据",
+      trigger: "change",
+    },
+  ],
+});
+const locale = reactive(zhCn);
+const renderText = (str) => {
+  if (str && [",", "，"].includes(str.charAt(0))) {
+    str = str.slice(1);
+  }
+  return str || "";
+};
+const copyToClipboard = (text) => {
+  var textarea = document.createElement("textarea"); //创建临时的textarea元素
+  textarea.value = text; //设置要复制的文本内容
+  document.body.appendChild(textarea); //添加到页面上
+  textarea.select(); //选中文本区域
+  try {
+    var successful = document.execCommand("copy"); //执行复制命令
+    var msg = successful ? "成功" : "失败";
+    console.log("已复制到剪贴板", msg);
+  } catch (err) {
+    console.error("无法复制到剪贴板", err);
+  } finally {
+    document.body.removeChild(textarea); //移除临时的textarea元素
+  }
+};
+const onSubmit = async (formEl) => {
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      const { dateString, text } = form;
+      const cleanedText = text.replace(/\n/g, "");
+
+      const meals = cleanedText.split(/星期[一二三四五]/).filter(Boolean);
+
+      const days = ["monday", "tuesday", "wednesday", "thursday", "friday"];
+      const menu = {
+        monday: {},
+        tuesday: {},
+        wednesday: {},
+        thursday: {},
+        friday: {},
+      };
+
+      meals.forEach((meal, index) => {
+        const day = days[index];
+        const sections = meal.split(/早餐|午餐|晚餐/).filter(Boolean);
+        menu[day] = {
+          [`${day}Breakfast`]: renderText(sections[0]?.trim()),
+          [`${day}Lunch`]: renderText(sections[1]?.trim()),
+          [`${day}Supper`]: renderText(sections[2]?.trim()),
+        };
+      });
+      const { monday, tuesday, wednesday, thursday, friday } = menu;
+
+      let date = new Date(dateString);
+      const result = {};
+
+      for (var i = 0; i <= 5; i++) {
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        result[`${days[i]}Date`] = `${month}月${day}日`;
+        date.setDate(date.getDate() + 1);
+      }
+
+      const data = {
+        ...result,
+        ...monday,
+        ...tuesday,
+        ...wednesday,
+        ...thursday,
+        ...friday,
+      };
+      const json = JSON.stringify(data);
+      form.data = json;
+      ElMessage("已成功生成数据，请复制后点击跳转按钮！");
+      copyToClipboard(json)
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+};
+
+const handleTo = () => {
+  window.open("https://docxtemplater.com/demo/#/view/cyber-security-report");
+};
+</script>
